7dd0e7107c96e97bb341038266bc18c7400d84e3
Shuai Lin <linshuai2012@gmail.com>
2015-12-27 16:12:27 +0800

    clang-format c_bpwrapper.cpp

--- c_bpwrapper.cpp
+++ c_bpwrapper.cpp
@@ -1,23 +1,23 @@
+#include <client/linux/handler/exception_handler.h>
+#include <client/linux/handler/minidump_descriptor.h>
 #include <stdio.h>
+
 #include "c_bpwrapper.h"
-#include "client/linux/handler/exception_handler.h"
-#include "client/linux/handler/minidump_descriptor.h"
 
 bool DumpCallback(const google_breakpad::MinidumpDescriptor& descriptor,
                   void* context,
-                  bool succeeded) 
+                  bool succeeded)
 {
     printf("Dump path: %s\n", descriptor.path());
     return succeeded;
 }
 
-extern "C" 
+extern "C" {
+CBPWrapperExceptionHandler newCBPWrapperExceptionHandler(void)
 {
-    CBPWrapperExceptionHandler newCBPWrapperExceptionHandler(void)
-    {
-        printf("init Simple breakpad\n");
-        google_breakpad::MinidumpDescriptor descriptor(".");
-        return reinterpret_cast<void*>(new google_breakpad::ExceptionHandler(descriptor, NULL, DumpCallback,
-                                       NULL, true, -1));
-    }
+    printf("init Simple breakpad\n");
+    google_breakpad::MinidumpDescriptor descriptor(".");
+    return reinterpret_cast<void*>(new google_breakpad::ExceptionHandler(
+        descriptor, NULL, DumpCallback, NULL, true, -1));
+}
 }
